ce8550d6158d3b18eab8537de1a1f60fad467100
Arber Topalli <atopalli@c1r3p8.42quebec.com>
2022-06-11 14:02:11 -0400

    update

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,14 @@
 /*   By: atopalli <atopalli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/11 13:24:25 by atopalli          #+#    #+#             */
-/*   Updated: 2022/06/11 13:47:24 by atopalli         ###   ########.fr       */
+/*   Updated: 2022/06/11 13:58:54 by atopalli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_h.h"
+
+static void	ft_putchar(char c, int *counter);
+static void	ft_putstr(const char *str, int *counter);
 
 int	ft_printf(const char *format, ...)
 {
@@ -24,7 +27,7 @@ int	ft_printf(const char *format, ...)
 		if (*format == '%' && format++)
 		{
 			if (*format == 's' && format++)
-				ft_putstr(va_arg(ap, char*), &counter);
+				ft_putstr(va_arg(ap, char *), &counter);
 			else if (*format == 'c' && format++)
 				ft_putchar(va_arg(ap, int), &counter);
 			else if (*format == '%' && format++)
@@ -33,8 +36,22 @@ int	ft_printf(const char *format, ...)
 			{
 				if (*format == 'p')
 					ft_putstr("0x", &counter);
-				ft_putstr(ft_convert(va_arg(ap, long int), *format++), &counter);
+				ft_putstr(ft_convert(va_arg(ap, long int),
+						*format++), &counter);
 			}
 		}
 	}
+	return (counter);
+}
+
+static void	ft_putstr(const char *str, int *counter)
+{
+	while (*str)
+		ft_putchar(*str++, counter);
+}
+
+static void	ft_putchar(char c, int *counter)
+{
+	write(1, &c, 1);
+	*counter += 1;
 }
